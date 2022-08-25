f64328af60744e5ccdcda10a5c887fcbbc62b45c
Powell <preston.powell@nebo.edu>
2022-08-25 15:04:04 -0600

    Player Movement and Camera Follow

--- First Project/Assets/Scripts/PlayerController.cs
+++ First Project/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public string playerName = "Rob Boss";
+    
     public int speed; // set speed value
-    public float hInput; // horizontal input
-    public float vInput; //vertical input
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float hInput; // store horizontal input from keyboard
+    public float vInput; //store vertical input from keyboard
+    
     // Update is called once per frame
     void Update()
     {
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
-
+        // This is the code that moves the player
         transform.Translate(Vector3.right * speed * hInput * Time.deltaTime);// Left and Right movement
-
+            
         transform.Translate(Vector3.forward * speed * vInput * Time.deltaTime);//Forward and Backward movement
     }
 }
