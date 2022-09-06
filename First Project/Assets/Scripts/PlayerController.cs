d91188539d6f2866f35acd7763d4d6ec94241331
Powell <preston.powell@nebo.edu>
2022-09-06 09:19:30 -0600

    Update

--- First Project/Assets/Scripts/PlayerController.cs
+++ First Project/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float rotSpeed; // Set rotation speed
     public float hInput; // Store horizontal input from keyboard
     public float vInput; //Store vertical input from keyboard
+    public float jumpForce = 10f;
+    public Rigidbody playerRB; // Reference Player Rigidbody
     
     // Update is called once per frame
     void Update()
@@ -19,5 +21,7 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(Vector3.up, rotSpeed * hInput * Time.deltaTime);
         // Left and Right rotation            
         transform.Translate(Vector3.forward * speed * vInput * Time.deltaTime);//Forward and Backward movement
+        if(Input.GetKeyDown(KeyCode.Space))
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
