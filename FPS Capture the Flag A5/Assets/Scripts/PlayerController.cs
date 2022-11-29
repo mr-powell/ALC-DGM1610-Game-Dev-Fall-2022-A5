2767933252f863aaa47bd003bfde7ff686a8145b
Powell <preston.powell@nebo.edu>
2022-11-29 14:05:03 -0700

    Created FPS Project and FPS Controller

---
+++ FPS Capture the Flag A5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Stats")]
+    public float moveSpeed;
+    public float jumpForce;
+    [Header("Camera Info")]
+    public float lookSensitivity;
+    public float maxLookX;
+    public float minLookX;
+    // Private variables
+    private float rotX;
+    private Camera camera;
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        //Get Components
+        camera = Camera.main;
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      PlayerMove();
+      CameraLook();
+
+      if(Input.GetButtonDown("Jump"))
+        PlayerJump();   
+    }
+
+    void PlayerMove()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;//Get input for left and right movement
+        float z = Input.GetAxis("Vertical") * moveSpeed;// Get input for forward and back movement
+
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir.y = rb.velocity.y;    
+        
+        rb.velocity = dir;// Drives movement relative to the cameras look direction
+    }
+
+    void CameraLook()
+    {
+        float y = Input.GetAxis("Mouse X") * lookSensitivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);// Clamp rotation on the X Axis
+
+        camera.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;// Drives Camera Look Rotation
+    }
+
+    void PlayerJump()
+    {
+        //Ground Check
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if(Physics.Raycast(ray, 1.1f))
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
