e6f68ae9b64d528b10b0ab47dbf92a8321205454
Powell <preston.powell@nebo.edu>
2022-12-01 14:01:38 -0700

    Added Health & Ammo Pickup

--- FPS Capture the Flag A5/Assets/Scripts/PlayerController.cs
+++ FPS Capture the Flag A5/Assets/Scripts/PlayerController.cs
@@ -71,4 +71,14 @@ public class PlayerController : MonoBehaviour
         if(Physics.Raycast(ray, 1.1f))
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
+
+    public void GiveHealth(int amount)
+    {
+        Debug.Log("Player picked up health!");
+    }
+
+    public void GiveAmmo(int amount)
+    {
+        Debug.Log("Player picked up ammo!");
+    }
 }
